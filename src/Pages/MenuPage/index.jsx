6aae599bacffcdb16a1464b281d4ec091e736b01
mjmnw <martinjoman2907@gmail.com>
2023-09-23 01:18:20 +0700

    Test

--- src/Pages/MenuPage/index.jsx
+++ src/Pages/MenuPage/index.jsx
@@ -2,27 +2,35 @@ import CategoryBar from "../../Components/CatregoryBar/CategoryBar";
 import Navbar from "../../Components/Navbar/Navbar";
 import ProductCard from "../../Components/ProductCard/ProductCard";
 import Sidebar from "../../Components/SideBar/SideBar";
-import TransactionBar from "../../Components/TransactionBar/TransactionBar"
+import TransactionBar from "../../Components/TransactionBar/TransactionBar";
+import MultiActionAreaCard from "../../Components/ProductBar";
 
 export default function LandingPage() {
     return (
         <div className="">
-            <Sidebar/>
-            <Navbar/>
-            <TransactionBar/>
-           
-            <main className="w-[cacl(100%-3.37rem)] ml-auto bg-black">
-                <div className="2xl:container mx-auto space-y-6">
-                    <div className="h-16 "></div>
-                    <div className="px-6 lg:px-12 mx-6">
-                        <div className="h-96 flex justify-center items-center rounded-2xl border border-dashed border-gray-300/40">
-                            <span className="text-gray-900">
-                                <ProductCard/>
-                            </span>
-                        </div>
-                    </div>
+            <Sidebar />
+            <Navbar />
+            <TransactionBar />
+            <div className="border ml-20 flex w-[1075px] h-[590px] flex-col gap-5 overflow-y-scroll p-3">
+                <div className="flex h-[250px] gap-10">
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
                 </div>
-            </main>
+                <div className="flex h-[250px] gap-10">
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
+                </div>
+                <div className="flex h-[250px] gap-10">
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
+                    <MultiActionAreaCard />
+                </div>
+            </div>
         </div>
     );
 }
